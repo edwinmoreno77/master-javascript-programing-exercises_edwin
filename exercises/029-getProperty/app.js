9a5e2b8c5645c224465bb1c61dc879ad41e0e03f
Edwin Moreno <edwinmoreno77@gmail.com>
2024-09-23 21:51:11 -0300

    getProperty finished

--- exercises/029-getProperty/app.js
+++ exercises/029-getProperty/app.js
@@ -1,4 +1,13 @@
 function getProperty(obj, key) {
   // your code here
-  
+  return obj[key];
+
 }
+
+const person = {
+  name: 'Pedro',
+  age: 35,
+  country: 'Chile'
+}
+
+getProperty(person, 'name')
