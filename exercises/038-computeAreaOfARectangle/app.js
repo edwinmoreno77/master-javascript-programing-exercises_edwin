d3af3ac340f2c1b3b833f5d3f7680a75a7e070bb
Edwin Moreno <edwinmoreno77@gmail.com>
2024-11-02 18:06:54 -0300

    computeAreaOfARectangle finished

--- exercises/038-computeAreaOfARectangle/app.js
+++ exercises/038-computeAreaOfARectangle/app.js
@@ -1,7 +1,8 @@
 function computeAreaOfARectangle(length, width) {
   // your code here
-
+  let area = length * width;
+  return area;
 }
 
 let output = computeAreaOfARectangle(10, 18);
-console.log(output) // --> 180
+console.log(output); // --> 180
