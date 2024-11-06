117ea701ba6a456969748b8eff4cbe0c15877212
Edwin Moreno <edwinmoreno77@gmail.com>
2024-11-05 23:24:19 -0300

    computePerimeterOfARectangle finished

--- exercises/039-computePerimeterOfARectangle/app.js
+++ exercises/039-computePerimeterOfARectangle/app.js
@@ -1,5 +1,7 @@
 function computePerimeterOfARectangle(length, width) {
   // your code here
+  let perimeter = 2 * (length + width);
+  return perimeter;
 }
 
 let output = computePerimeterOfARectangle(5, 2);
