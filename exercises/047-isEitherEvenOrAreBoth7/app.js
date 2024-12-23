c710af58e2298238e24688a3d256075d7966431a
Edwin Moreno <edwinmoreno77@gmail.com>
2024-12-23 19:18:28 -0300

    isEitherEvenOrAreBoth7 finished

--- exercises/047-isEitherEvenOrAreBoth7/app.js
+++ exercises/047-isEitherEvenOrAreBoth7/app.js
@@ -1,4 +1,11 @@
 function isEitherEvenOrAreBoth7(num1, num2) {
     // your code here
-    
+    const isEitherEven = (num1 % 2 == 0) || (num2 % 2 == 0)
+    const areBoth7 = (num1 == 7 && num2 == 7)
+    const result = isEitherEven || areBoth7;
+
+    return result;
 }
+
+console.log(isEitherEvenOrAreBoth7(3, 7));
+
