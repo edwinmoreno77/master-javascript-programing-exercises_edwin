6f3d8992705960ae8de36779fa9ac61d8b399f50
Edwin Moreno <edwinmoreno77@gmail.com>
2024-12-31 00:26:38 -0300

    isEitherEvenAndLessThan9 finished

--- exercises/048-isEitherEvenAndLessThan9/app.js
+++ exercises/048-isEitherEvenAndLessThan9/app.js
@@ -1,6 +1,10 @@
 function isEitherEvenAndLessThan9(num1, num2) {
     // your code here
+    const isEitherEven = num1 % 2 == 0 || num2 % 2 == 0;
+    const lessThan9 = num1 < 9 && num2 < 9;
+    const result = isEitherEven && lessThan9;
 
+    return result;
 }
 
 let output = isEitherEvenAndLessThan9(2, 4);
