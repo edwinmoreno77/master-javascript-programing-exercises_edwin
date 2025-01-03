8e86cf6cc29719fe5e95dc9007eb3a8561172b73
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-02 21:08:35 -0300

    computeAverageOfNumbers finished

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,15 @@
 // Write your function here
+
+function computeAverageOfNumbers(numArray) {
+
+    if (numArray.length == 0) return 0;
+
+    let sumOfArray = 0;
+    for (num of numArray) {
+        sumOfArray += num;
+    }
+    const averegaOfNumbers = sumOfArray / numArray.length
+    return averegaOfNumbers;
+}
+
+
