0486ccce2b8c1b4400c7de1daa66999c391ac801
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-10 15:08:29 -0300

    getElementsAfter finished

--- exercises/056-getElementsAfter/app.js
+++ exercises/056-getElementsAfter/app.js
@@ -1,7 +1,9 @@
 function getElementsAfter(array, n) {
   // your code here
-  
+  const arrayAfter = array.slice(n + 1)
+  return arrayAfter;
+
 }
 
-let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2); 
+let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2);
 console.log(output); // --> ['d', 'e']
