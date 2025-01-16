5878505f0efb8b8fdafa0a340cf56eb65d064c60
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-16 15:06:31 -0300

    countCharacter finished

--- exercises/062-countCharacter/app.js
+++ exercises/062-countCharacter/app.js
@@ -1,6 +1,12 @@
 function countCharacter(str, char) {
     // your code here
-    
+    let count = 0;
+    for (i of str) {
+        if (i === char) count++
+    }
+
+    return count;
+
 }
 
 let output = countCharacter('I am a hacker', 'a');
