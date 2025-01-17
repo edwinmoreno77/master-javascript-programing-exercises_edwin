dfb6b7d94faf3c7074d2e569b9b66b2d8834af5f
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-17 08:41:03 -0300

    getAllLetters finished

--- exercises/063-getAllLetters/app.js
+++ exercises/063-getAllLetters/app.js
@@ -1,6 +1,8 @@
 function getAllLetters(str) {
     // your code here
-    
+    const letters = [];
+    for (letter of str) letters.push(letter)
+    return letters;
 }
 
 let output = getAllLetters('Radagast');
