084b7c11ad7acddab8f4b3889aa73799794ec5ac
Edwin Moreno <edwinmoreno77@gmail.com>
2025-01-31 22:01:04 -0300

    removeEvenValues finished

--- exercises/070-removeEvenValues/app.js
+++ exercises/070-removeEvenValues/app.js
@@ -1,12 +1,17 @@
 function removeEvenValues(obj) {
-    // your code here
-    
+  // your code here
+  for (key in obj) {
+    if (obj[key] % 2 == 0) {
+      delete obj[key]
+    }
+  }
+
 }
 
 let obj = {
   a: 2,
   b: 3,
-  c: 4
+  c: 4,
 };
 removeEvenValues(obj);
 console.log(obj); // --> { b: 3 }
