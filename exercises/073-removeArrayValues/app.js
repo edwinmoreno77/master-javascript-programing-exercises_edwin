9bb642f06b962e6c6ecfbbe9b8977fdfd5e641cf
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-03 15:25:51 -0300

    removeArrayValues finished

--- exercises/073-removeArrayValues/app.js
+++ exercises/073-removeArrayValues/app.js
@@ -1,4 +1,10 @@
 function removeArrayValues(obj) {
     // your code here
-    
+    for (key in obj) {
+        if (Array.isArray(obj[key])) {
+            delete obj[key]
+        }
+    }
 }
+
+removeArrayValues({ a: 1, b: 2, c: { a: 4, g: 'deded' } })
