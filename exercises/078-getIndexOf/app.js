275111c130dfb0d2a55d5e2b2be7a1868169efb2
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-06 08:36:10 -0300

    gitIndexOf finished

--- exercises/078-getIndexOf/app.js
+++ exercises/078-getIndexOf/app.js
@@ -1,1 +1,9 @@
 // Write your function here
+function getIndexOf(letter, string) {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == letter) return i;
+  }
+  return -1;
+}
+
+console.log(getIndexOf("a", "I am a hacker"));
