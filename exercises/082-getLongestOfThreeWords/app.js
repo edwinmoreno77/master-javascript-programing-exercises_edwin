d7af413351a2525be83dff1def7f80b9f5f0e036
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-08 13:34:35 -0300

    getLongestOfThreeWords finished

--- exercises/082-getLongestOfThreeWords/app.js
+++ exercises/082-getLongestOfThreeWords/app.js
@@ -1,7 +1,15 @@
 function getLongestOfThreeWords(word1, word2, word3) {
-    // your code here
-    
+  // your code here
+  const arrayOfWords = [word1, word2, word3];
+  let longestWord = "";
+
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    if (arrayOfWords[i].length > longestWord.length) {
+      longestWord = arrayOfWords[i];
+    }
+  }
+  return longestWord;
 }
 
-let output = getLongestOfThreeWords('these', 'three', 'words');
+let output = getLongestOfThreeWords("these", "three", "words");
 console.log(output); // --> 'these'
