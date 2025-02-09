decbaf3f6c42b11a52834466a13be264db6919c0
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-09 19:24:29 -0300

    findShortestOfThreeWords finished

--- exercises/083-findShortestOfThreeWords/app.js
+++ exercises/083-findShortestOfThreeWords/app.js
@@ -1,7 +1,10 @@
 function findShortestOfThreeWords(word1, word2, word3) {
-    // your code here
-    
+  // your code here
+  let result = word1;
+  if (word2 < result) result = word2;
+  if (word3 < result) result = word3;
+  return result;
 }
 
-let output = findShortestOfThreeWords('a', 'two', 'three');
+let output = findShortestOfThreeWords("a", "two", "three");
 console.log(output); // --> 'a'
