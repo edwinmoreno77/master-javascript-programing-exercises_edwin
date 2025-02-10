c33fd37f0b87c0e3e7bdb2748fc2302b03923c5e
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-10 07:30:20 -0300

    joinThreeArrays finished

--- exercises/084-joinThreeArrays/app.js
+++ exercises/084-joinThreeArrays/app.js
@@ -1,4 +1,7 @@
 function joinThreeArrays(arr1, arr2, arr3) {
   // your code here
-  
+  return arr1.concat(arr2, arr3);
 }
+
+const output = joinThreeArrays([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(output);
