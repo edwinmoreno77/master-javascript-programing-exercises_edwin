280bcc91b86ec8bb427144c4cdef5ffceac29e3a
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-13 13:35:46 -0300

    removeElement finished

--- exercises/089-removeElement/app.js
+++ exercises/089-removeElement/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+function removeElement(arr, element) {
+  const result = arr.filter((item) => item != element);
+  return result;
+}
+
+const output = removeElement([1, 2, 3, 4, 5, 6], 5);
+console.log(output);
