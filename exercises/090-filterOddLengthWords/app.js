9d590e135f5018f8f74dfff726849bc145a5ac28
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-14 14:49:39 -0300

    090-filterOddLengthWords finished

--- exercises/090-filterOddLengthWords/app.js
+++ exercises/090-filterOddLengthWords/app.js
@@ -1,7 +1,7 @@
 function filterOddLengthWords(words) {
-    // your code here
-    
+  // your code here
+  return words.filter((word) => word.length % 2 != 0);
 }
 
-let output = filterOddLengthWords(['there', 'it', 'is', 'now']);
+let output = filterOddLengthWords(["there", "it", "is", "now"]);
 console.log(output); // --> ['there', 'now']
