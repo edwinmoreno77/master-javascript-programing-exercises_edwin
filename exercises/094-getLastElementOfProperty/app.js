c14713274b675b320dd0a9c1453a8429eedfc4bf
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-27 16:58:25 -0300

    getLastElementOfProperty finished

--- exercises/094-getLastElementOfProperty/app.js
+++ exercises/094-getLastElementOfProperty/app.js
@@ -1,1 +1,15 @@
 // Write your function here
+
+function getLastElementOfProperty(obj, key) {
+  if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length === 0) {
+    return undefined;
+  }
+  const index = obj[key].length - 1;
+  return obj[key][index];
+}
+
+let obj = {
+  key: [1, 2, 5],
+};
+let output = getLastElementOfProperty(obj, "key");
+console.log(output); // --> 5
