c0290644b38a13b04d91fbef65c19222e2fe32f0
Edwin Moreno <edwinmoreno77@gmail.com>
2025-02-28 12:19:41 -0300

    getElementThatEqual10AtProperty finished

--- exercises/095-getElementsThatEqual10AtProperty/app.js
+++ exercises/095-getElementsThatEqual10AtProperty/app.js
@@ -1,1 +1,14 @@
 // Write your function here
+
+function getElementsThatEqual10AtProperty(obj, key) {
+  if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length == 0) return [];
+
+  const newArray = [...obj[key]];
+  return newArray.filter((item) => item === 10);
+}
+
+let obj = {
+  key: [1000, 10, 50, 10],
+};
+let output = getElementsThatEqual10AtProperty(obj, "key");
+console.log(output); // --> [10, 10]
