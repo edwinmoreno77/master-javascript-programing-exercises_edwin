54680dea1d4692ea7313d42df12f6a923adc3877
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-03 18:18:51 -0300

    getElementGraterThanTenAtProperty finshed

--- exercises/097-getElementsGreaterThanTenAtProperty/app.js
+++ exercises/097-getElementsGreaterThanTenAtProperty/app.js
@@ -1,1 +1,12 @@
 // Write your function here
+
+function getElementsGreaterThan10AtProperty(obj, key) {
+  if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length === 0) return [];
+  return obj[key].filter((num) => num > 10);
+}
+
+let obj = {
+  key: [1, 20, 30],
+};
+let output = getElementsGreaterThan10AtProperty(obj, "key");
+console.log(output); // --> [20, 30]
