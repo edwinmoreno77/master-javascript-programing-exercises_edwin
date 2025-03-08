413a09794d62e9651bbef424d8140d02402779c7
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-08 13:04:44 -0300

    arrayToObject finished

--- exercises/142.1-ArrayToObject/app.js
+++ exercises/142.1-ArrayToObject/app.js
@@ -1,7 +1,7 @@
 function transformFirstAndLast(array) {
   // your code here
-  
+  return { [array[0]]: array[array.length - 1] };
 }
 
-let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
+let arr = ["Queen", "Elizabeth", "Of Hearts", "Beyonce"];
 console.log(transformFirstAndLast(arr)); // --> { Queen: "Beyonce" }
