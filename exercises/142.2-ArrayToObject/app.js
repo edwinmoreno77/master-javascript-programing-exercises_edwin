7e747f14f0d547a049b19e288aeb025d209023f5
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-11 10:08:19 -0300

    ArrayToObject finished

--- exercises/142.2-ArrayToObject/app.js
+++ exercises/142.2-ArrayToObject/app.js
@@ -1,7 +1,14 @@
 function fromListToObject(array) {
   // your code here
-  
+  return array.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
 }
 
-let output = fromListToObject([['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]])
-console.log(output) // --> { make: 'Ford', model: 'Mustang', year: 1964 }
+let output = fromListToObject([
+  ["make", "Ford"],
+  ["model", "Mustang"],
+  ["year", 1964],
+]);
+console.log(output); // --> { make: 'Ford', model: 'Mustang', year: 1964 }
