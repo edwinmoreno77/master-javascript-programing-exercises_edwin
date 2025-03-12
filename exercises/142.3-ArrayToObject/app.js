943fb69010ccc5ebc9f12995b3f8a17a3c7bd695
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-12 09:55:17 -0300

    ArrayToObject finished

--- exercises/142.3-ArrayToObject/app.js
+++ exercises/142.3-ArrayToObject/app.js
@@ -1,4 +1,26 @@
 function transformEmployeeData(array) {
   // your code here
-  
+  return array.map((person) => {
+    person.reduce((acc, [key, value]) => {
+      if (!acc[key]) acc[key] = value;
+      return acc;
+    }, {});
+  });
 }
+
+let output = transformEmployeeData([
+  [
+    ["firstName", "Joe"],
+    ["lastName", "Blow"],
+    ["age", 42],
+    ["role", "clerk"],
+  ],
+
+  [
+    ["firstName", "Mary"],
+    ["lastName", "Jenkins"],
+    ["age", 36],
+    ["role", "manager"],
+  ],
+]);
+console.log(output); // --> [{ firstName: 'Joe', lastName: 'Blow', age: 42, role: 'clerk' }, { firstName: 'Mary', lastName: 'Jenkins', age: 36, role: 'manager' }]
