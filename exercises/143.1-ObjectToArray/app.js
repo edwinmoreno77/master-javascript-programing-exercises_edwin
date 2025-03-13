01be51adcd9d6ef4c8cd59e93e11c43b28e56d7d
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-13 11:21:07 -0300

    ObjectToArray finished

--- exercises/143.1-ObjectToArray/app.js
+++ exercises/143.1-ObjectToArray/app.js
@@ -1,7 +1,9 @@
 function getAllKeys(obj) {
   // your code here
-  
+  let aux = [];
+  for (let key in obj) aux.push(key);
+  return aux;
 }
 
-let output = getAllKeys({name: "Sam", age: 25, hasPets: true});
+let output = getAllKeys({ name: "Sam", age: 25, hasPets: true });
 console.log(output); // --> [ 'name', 'age', 'hasPets' ]
