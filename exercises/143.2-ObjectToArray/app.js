6b3e88681cf1d26f9780c61ca189295f1ec60099
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-14 10:43:31 -0300

    ObjectToArray finished

--- exercises/143.2-ObjectToArray/app.js
+++ exercises/143.2-ObjectToArray/app.js
@@ -1,7 +1,7 @@
 function listAllValues(obj) {
   // your code here
-  
+  return Object.values(obj);
 }
 
-let output = listAllValues({ name: 'Sam', age: 25, hasPets: true });
+let output = listAllValues({ name: "Sam", age: 25, hasPets: true });
 console.log(output); // --> [ 'Sam', 25, true ]
