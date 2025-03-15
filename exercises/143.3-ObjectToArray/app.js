988959d5863ffb38329830954919c9e04ff43bc7
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-15 20:11:13 -0300

    objectToArray finished

--- exercises/143.3-ObjectToArray/app.js
+++ exercises/143.3-ObjectToArray/app.js
@@ -1,7 +1,8 @@
 function convertObjectToList(obj) {
   // your code here
-  
+  return Object.entries(obj);
 }
 
-let output = convertObjectToList({ name: 'Holly', age: 35, role: 'producer' });
+let output = convertObjectToList({ name: "Holly", age: 35, role: "producer" });
 console.log(output); // --> [ ['name', 'Holly'], ['age', 35], ['role', 'producer'] ]
+
