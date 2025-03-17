683756c0a84d2053514ff808eebc1a666326cc1e
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-17 19:50:51 -0300

    getStringLength

--- exercises/144-getStringLength/app.js
+++ exercises/144-getStringLength/app.js
@@ -1,7 +1,9 @@
 function getStringLength(string) {
-    // your code here
-    
+  // your code here
+  let count = 0;
+  for (let __ of string) count += 1;
+  return count;
 }
 
-let output = getStringLength('hello');
+let output = getStringLength("hello");
 console.log(output); // --> 5
