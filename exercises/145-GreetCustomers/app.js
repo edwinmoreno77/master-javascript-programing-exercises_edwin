6272f51e322fc0a2e86ebdbb5f9a18d8dde8794a
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-22 11:39:57 -0300

    GreetCustomers finished

--- exercises/145-GreetCustomers/app.js
+++ exercises/145-GreetCustomers/app.js
@@ -1,23 +1,29 @@
 let customerData = {
-  'Joe': {
-    visits: 1
+  Joe: {
+    visits: 1,
   },
-  'Carol': {
-    visits: 2
+  Carol: {
+    visits: 2,
   },
-  'Howard': {
-    visits: 3
+  Howard: {
+    visits: 3,
   },
-  'Carrie': {
-    visits: 4
-  }
+  Carrie: {
+    visits: 4,
+  },
 };
 
 function greetCustomer(firstName) {
-  let greeting = '';
   // your code here
-	
-  return greeting;
+  const customer = customerData[firstName];
+
+  if (!customer) return "Welcome! Is this your first time?";
+
+  if (customer.visits === 1) {
+    return `Welcome back, ${firstName}! We're glad you liked us the first time!`;
+  }
+
+  return `Welcome back, ${firstName}! So glad to see you again!`;
 }
 
-console.log(greetCustomer('Joe')); // --> 'Welcome back, Joe! We're glad you liked us the first time!'
+console.log(greetCustomer("Joe")); // --> 'Welcome back, Joe! We're glad you liked us the first time!'
