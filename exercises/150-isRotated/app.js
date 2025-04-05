a228e4f604c684ef7d86c252ca34b430e1bfcf1d
Edwin Moreno <edwinmoreno77@gmail.com>
2025-04-05 11:39:36 -0300

    Implement isRotated function to check if one string is a rotation of another

--- exercises/150-isRotated/app.js
+++ exercises/150-isRotated/app.js
@@ -1,8 +1,9 @@
 function isRotated(str1, str2) {
-    // your code here
-    
-  }
-  
-  console.log(isRotated('hello world', 'orldhello w')); // --> true
-  console.log(isRotated('hello world', 'omrel wp')); // --> false
-  
+  // your code here
+  if (str1.length != str2.length) return false;
+  const concatStr = str1 + str2;
+  return concatStr.includes(str2);
+}
+
+console.log(isRotated("hello world", "orldhello w")); // --> true
+console.log(isRotated("hello world", "omrel wp")); // --> false
