51de4ff3707eb6b8b3cbe8ffa2d09fe51b3be608
Edwin Moreno <edwinmoreno77@gmail.com>
2025-04-06 09:14:03 -0400

    Implement binary search function with input validation

--- exercises/151-search/app.js
+++ exercises/151-search/app.js
@@ -1,7 +1,22 @@
 function search(array, value) {
-  // your code here
-  
+  if (!Array.isArray(array) || array.length < 1) return -1;
+
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (array[mid] === value) return mid;
+
+    if (array[mid] < value) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
 }
 
-let arr = [1, 3, 16, 22, 31, 33, 34]
-console.log(search(arr, 31)); // --> 4
+const arr = [1, 3, 16, 22, 31, 33, 34];
+console.log(search(arr, 31)); // 4
