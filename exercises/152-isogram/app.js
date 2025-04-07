a00421b18a72cd6ca81586df294067adf3baa9c5
Edwin Moreno <edwinmoreno77@gmail.com>
2025-04-07 13:12:52 -0400

    Implement isIsogram function to check if a string is an isogram

--- exercises/152-isogram/app.js
+++ exercises/152-isogram/app.js
@@ -1,7 +1,23 @@
 function isIsogram(text) {
   // your code here
-  
+  const str = text.toLowerCase();
+  if (str.length === 0) return true;
+  const arrayOfLetter = []
+  for (let i = 0; i < str.length; i++) {
+    if (arrayOfLetter.includes(str[i])) return false;
+    arrayOfLetter.push(str[i]);
+  }
+  return true;
 }
 
 console.log(isIsogram("Camile")); // --> true
 console.log(isIsogram("Camille")); // --> false
+
+// :::::::::::::::::::::::::::::::::::::::::
+// ::::::::::: Short function ::::::::::::::
+// :::::::::::::::::::::::::::::::::::::::::
+
+// function isIsogram(text) {
+//   const lowerText = text.toLowerCase();
+//   return new Set(lowerText).size === lowerText.length;
+// }
